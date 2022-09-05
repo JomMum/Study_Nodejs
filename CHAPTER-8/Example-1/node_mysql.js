b5254e961df801e771f4496e3839562967d1df65
JongWoong <78545700+JomMum@users.noreply.github.com>
2022-09-05 23:59:09 +0900

    CHAPTER 8
    [오늘 배운 것]
    -MYSQL 세팅
    -MYSQL 기초
    -MYSQL 모듈

--- CHAPTER-8/Example-1/node_mysql.js
+++ CHAPTER-8/Example-1/node_mysql.js
@@ -3,13 +3,14 @@ const mysql = require('mysql');
 
 //DB와 연결
 let client = mysql.createConnection({
+    host: 'localhost',
     user: 'root',
-    password: '1234'
+    password: 'tlschs427',
+    database: 'Company'
 });
 
 //DB 쿼리 사용
-client.query('USE Company');
 client.query('SELECT * FROM products', function(error, result, fields) {
-    if(error) { console.log('쿼리 문장에 오류가 있습니다.'); }
+    if(error) { console.log(error); }
     else { console.log(result); }
 });
